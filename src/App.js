8a02407f8c2d9a5f9e58efe73cc792d96e53b045
NgeekReyes <ng_reyes@outlook.com>
2021-04-16 14:22:55 -0500

    update...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import './App.css';
 import './style/body.css';
-import PortList from './Pages/Portafolio/PArchive/PortList';
-import AppRouter  from './routes/AppRouter';
-import Diplomas from './Pages/Reconocimiento/diplomas/Diplomas';
+//import PortList from './Pages/Portafolio/PArchive/PortList';
+//import AppRouter  from './routes/AppRouter';
+import Update from './Pages/update';
+//import Diplomas from './Pages/Reconocimiento/diplomas/Diplomas';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App (){
 
- 
-
-
   return (
     <Router>
-    
      <div className="App">
+       
      <Switch>
-        <Route path="/" exact component={AppRouter}></Route>
-        <Route path="/portlist" component={PortList}></Route>
-        <Route path="/diplomas" component={Diplomas}></Route>
+       
+        <Route path="/portlist" component={Update}></Route>
+        
       </Switch>
      </div>
     </Router>
---
+++ src/Pages/update.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Update = () => {
+
+    return (
+    <div className='App-header'>
+        <h1>UPDATE.......</h1>
+    </div>
+    );
+};
+
+export default Update;
