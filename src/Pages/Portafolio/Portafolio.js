68462363805a2b683cdb28fba1289cdbe3ca9f5c
NgeekReyes <ng_reyes@outlook.com>
2021-04-20 16:28:47 -0500

    add new project

--- src/Pages/Portafolio/Portafolio.js
+++ src/Pages/Portafolio/Portafolio.js
@@ -10,10 +10,18 @@ const Portafolio = () => {
 
     return (
     <motion.div className='portafolio' initial={{opacity:0, y: 40}} animate={{opacity:1, y: 0 }}>
-            <Row >
+           <div>
+           <Row >
                 <Col><motion.div whileHover={{scale: 1.04}} className="card-img"></motion.div></Col>
                 <Col><motion.div whileHover={{scale: 1.04}} className="card-img2"></motion.div></Col>
             </Row>
+           </div>
+           <div className="demo">
+           <Row >
+                <Col><motion.div whileHover={{scale: 1.04}} className="card-img4"></motion.div></Col>
+                <Col><motion.div whileHover={{scale: 1.04}} className="card-img5"></motion.div></Col>
+            </Row>
+           </div>
           
     </motion.div>
 
