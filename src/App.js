74dc9e23a8b8fd4736cd390a9b0faea3662ec1b4
NGEEK23 <ng_reyes@outlook.com>
2021-05-11 17:25:38 -0500

    delete pages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import './style/body.css';
 import PortList from './Pages/Portafolio/PArchive/PortList';
 import AppRouter  from './routes/AppRouter';
-
-import Diplomas from './Pages/Reconocimiento/diplomas/Diplomas';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App (){
 
@@ -16,7 +14,6 @@ function App (){
        
         <Route path="/" exact component={AppRouter}></Route>
         <Route path="/portlist" component={PortList}></Route>
-        <Route path="/diplomas" component={Diplomas}></Route>
         
       </Switch>
      </div>
