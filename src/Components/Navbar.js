93229c610697e035224a7f561e7568ab0e7094ce
NGEEK23 <ng_reyes@outlook.com>
2021-05-12 13:05:51 -0500

    new interface navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,25 @@
 import React, {Component} from 'react';
-import '../style/Navbar.css';
-//import ng from '../Assets/ng.png';
+import '../Components/Navbar.css';
+import ng from '../Assets/ng.png';
 //import men from '../Assets/men.svg';
 //import { Link } from 'react-scroll';
-
+import Logo from '../Components/Logo';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 class Navbars extends Component {
 
     render(){
         return(
-          <div className='effect'>    
+          <div className='effect'>   
             <Navbar collapseOnSelect  expand='lg' className='back'>
               <Container>
                 <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                   <Navbar.Collapse id='responsive-navbar-nav'>
                   <Nav className='nav-menu effect-nav'>
-                      <Nav.Link href='/about'><span data-hover='ABOUT'>About</span></Nav.Link>
-                      <Nav.Link href='/'><span data-hover='BOOK'>Portfolio</span></Nav.Link>
-                      <Nav.Link href='/rewards'><span data-hover='REWARDS'>Rewards</span></Nav.Link>
-                      <Nav.Link href='/contact'><span data-hover='CONTACT'>Contact</span></Nav.Link>
+                      <Nav.Link href='/about'><span data-hover='ABOUT'>ABOUT</span></Nav.Link>
+                      <Nav.Link href='/'><span data-hover='PORTFOLIO'>PORTFOLIO</span></Nav.Link>
+                      <Nav.Link href='/rewards'><span data-hover='REWARDS'>REWARDS</span></Nav.Link>
+                      <Nav.Link href='/contact'><span data-hover='CONTACT'>CONTACT</span></Nav.Link>
                   </Nav>
                   </Navbar.Collapse>
               </Container>
