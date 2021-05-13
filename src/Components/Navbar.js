d16a2926991759ea64b5c3edc6d470af8122bb14
NGEEK23 <ng_reyes@outlook.com>
2021-05-13 11:16:32 -0500

    add new structure grid

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,13 +9,17 @@ class Navbars extends Component {
           <div className='effect'>   
             <Navbar collapseOnSelect  expand='lg' className='back'>
               <Container>
+                <div className="effect-nav lgoeffect">
+                <Nav.Link href='/'><span data-hover='Reyes'>Ng</span></Nav.Link>
+                </div>
                 <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                   <Navbar.Collapse id='responsive-navbar-nav'>
                   <Nav className='nav-menu effect-nav'>
-                      <Nav.Link href='/about'><span data-hover='ABOUT'>ABOUT</span></Nav.Link>
+                  
+                      <Nav.Link href='/'><span data-hover='ABOUT'>ABOUT</span></Nav.Link>
                       <Nav.Link href='/'><span data-hover='PORTFOLIO'>PORTFOLIO</span></Nav.Link>
-                      <Nav.Link href='/rewards'><span data-hover='REWARDS'>REWARDS</span></Nav.Link>
-                      <Nav.Link href='/contact'><span data-hover='CONTACT'>CONTACT</span></Nav.Link>
+                      <Nav.Link href='/'><span data-hover='REWARDS'>REWARDS</span></Nav.Link>
+                      <Nav.Link href='/'><span data-hover='CONTACT'>CONTACT</span></Nav.Link>
                   </Nav>
                   </Navbar.Collapse>
               </Container>
--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -5,33 +5,30 @@ import './Portfolio.css';
 //import { Link } from 'react-router-dom';
 import { motion } from "framer-motion";
 import Logo from '../../Components/Logo';
-
+//import {AxityMobile, AxityWebP, AxityWebT, OmnibusMexico} from '../../../Data/data-proyectos';
 
 const Portafolio = () => {
 
     return (
-
-    <div className="portf">
-
-    <Logo/>
     <motion.div className='portafolio' initial={{opacity:0, y: 40}} animate={{opacity:1, y: 0 }}>
-       <div>
-       <Row >
-            <Col><motion.div whileHover={{scale: 1.04}} className="card-img"></motion.div></Col>
-            <Col><motion.div whileHover={{scale: 1.04}} className="card-img2"></motion.div></Col>
-        </Row>
-       </div>
-       <div className="demo">
-       <Row >
-            <Col><motion.div whileHover={{scale: 1.04}} className="card-img4"></motion.div></Col>
-            <Col><motion.div whileHover={{scale: 1.04}} className="card-img5"></motion.div></Col>
-        </Row>
-       </div>
+        <div className="portf">
+            <Logo/> 
+            <div>
+                <Row >
+                    <Col><motion.div whileHover={{scale: 1.04}} className="card-img"></motion.div></Col>
+                    <Col><motion.div whileHover={{scale: 1.04}} className="card-img2"></motion.div></Col>
+                </Row>
+            </div>
+            <div className="demo">
+                <Row >
+                    <Col><motion.div whileHover={{scale: 1.04}} className="card-img4"></motion.div></Col>   
+                </Row>
+            </div>
+    
+        </div>   
+
+
     </motion.div>
-    </div>   
-
-
-
         
 
 );
