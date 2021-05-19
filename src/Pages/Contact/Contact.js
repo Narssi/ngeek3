c088637af034f99818672ae2760b9bdb0c2d13f3
NGEEK23 <ng_reyes@outlook.com>
2021-05-19 11:48:05 -0500

    add contact

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -2,23 +2,24 @@ import React from 'react';
 import './Contact.css';
 import { ContactData } from "../../Data/data-ngk";
 import {  motion } from "framer-motion";
+//import { git } from "../../Assets/git.svg"
+import { Link } from 'react-router-dom';
 
 const Contacto = () => {
     return(
         <div className='Contacto'>
 
             <div>
-                <h1>Contacto</h1>
+                <h1>Contact</h1>
             </div>
 
             <div className="icons">
             {ContactData.map((icons, key) =>{
                         return(
 
-                            <li key={key} className="icons">
+                            <Link  to={{ pathname: "https://www.linkedin.com/in/ngeekreyes/" }} target="_blank" key={key} className="icons">
                                 <motion.img whileHover={{ scale: 0.8 }}  src={`./assets/Contact/${icons.id}.svg`} className="rounded float-left"  alt="img"/>
-
-                            </li>
+                            </Link>
                         );
                     })}
             </div>
